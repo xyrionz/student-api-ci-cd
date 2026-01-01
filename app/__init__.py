cc4bc0da3870ca345116d3dc38c7168fc452052a
Srivishnu P N <srivishnu22112004@gmail.com>
2026-01-01 17:07:39 +0000

    Fix flake8 F821 by explicit app import

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from app import routes  # noqa: E402,F401
+from app import routes  # noqa: F401,E402
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,4 +1,19 @@
+import app as flask_app
+
+
+def test_health():
+    client = flask_app.app.test_client()
+    response = client.get("/health")
+    assert response.status_code == 200
+
+
+def test_valid_student():
+    client = flask_app.app.test_client()
+    response = client.get("/student/1")
+    assert response.status_code == 200
+
+
 def test_invalid_student():
-    client = app.test_client()
+    client = flask_app.app.test_client()
     response = client.get("/student/-1")
     assert response.status_code == 404
