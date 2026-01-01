9c14d67cb5823769ba38717ea1b9fd56efd387c5
Srivishnu P N <srivishnu22112004@gmail.com>
2026-01-01 16:57:19 +0000

    Fix pytest test discovery issue

---
+++ tests/test_routes.py
@@ -0,0 +1,19 @@
+from app.routes import app
+import sys
+sys.path.append(".")
+
+
+def test_health():
+    client = app.test_client()
+    response = client.get("/health")
+    assert response.status_code == 200
+
+def test_valid_student():
+    client = app.test_client()
+    response = client.get("/student/1")
+    assert response.status_code == 200
+
+def test_invalid_student():
+    client = app.test_client()
+    response = client.get("/student/-1")
+    assert response.status_code == 400
